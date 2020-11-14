6e600fd4074073322cb280a5d73ff33b6a4b2be1
dhvanivora25 <dhvanivora95@gmail.com>
2020-11-13 21:47:41 -0800

    Everything other than confusion matrix Parameters still to be tuned

--- MHI Activity Classification/run_new.py
+++ MHI Activity Classification/run_new.py
@@ -10,6 +10,7 @@ from sklearn.metrics import confusion_matrix
 IN_DIR = "input_videos"
 OUT_DIR = "output"
 
+debug = True
 # Build dataset using MHIs and MEIs
 
 """
@@ -34,10 +35,12 @@ frames = {'boxing': [(0, 36), (36, 72), (72, 108)],
           'walking': [(18, 88), (242, 320), (441, 511)]}
 
 # Th needs to be chosen for each action
-Th = [1, 8, 10, 40, 5, 5]
+#Th = [1, 8, 10, 40, 5, 5]
+Th = [3,4,3,4,3,4]
 
 # T needs to be chosen and differs with actions
-T = [50, 30, 50, 50, 30, 10]
+#T = [50, 30, 50, 50, 30, 10]
+T = [50,50,50,50,50,50]
 
 # create all the action MHI, MEI, and labels
 def build_dataset(actions, frames):
@@ -91,14 +94,149 @@ def build_dataset(actions, frames):
 
     return labels, MHIs, MEIs, mus_mhi, vs_mhi, mus_mei, vs_mei
 
-labels, MHIs, MEIs, mus_mhi, vs_mhi, mus_mei, vs_mei = build_dataset(actions, frames)
 
-knn_cv = KNeighborsClassifier(n_neighbors=3)
+def build_entire_dataset(actions):
+    MHIs = []
+    MEIs = []
+    mus_mhi = []  # central moments
+    vs_mhi = []  # scale invariant moments
+    mus_mei = []  # central moments
+    vs_mei = []  # scale invariant moments
 
-# knn_cv.fit(vs_mhi, labels)
-#train model with cv of 5
-print(labels.shape)
-cv_scores = cross_val_score(knn_cv, vs_mhi, labels, cv=3)
+    labels = []
+
+    for i, action in enumerate(actions):
+        act = action[0]
+        num = action[1]
+
+        files_path = [x for x in os.listdir("./Data MHI/"+str(act))]
+
+        for j,file in enumerate(files_path):
+            if ".avi" in file:
+                cap = cv2.VideoCapture(os.path.join("./Data MHI/"+str(act)+"/",file))
+                length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                cap.release()
+                print(os.path.join("./Data MHI/" + str(act) + "/", file))
+                start = [1, int(length/3)+1, int(2*length/3)+1]
+                end = [int(length/3), int(2*length/3), length-1]
+                for iter in range(3):
+                    binaries, real = util.create_binary_images(os.path.join("./Data MHI/"+str(act)+"/",file), k=(5,) * 2, sigma=0,
+                                                               start_frame=start[iter], end_frame=end[iter],
+                                                               k_size=(3,) * 2, threshold=Th[i], folder=act + "_" + str(j))
+
+                    MHI = util.create_mhi(binaries, T[i])
+
+                    cv2.normalize(MHI, MHI, 0.0, 255.0, cv2.NORM_MINMAX)
+                    MEI = (255 * MHI > 0).astype(np.uint8)
+
+                    mu_mhi, v_mhi = util.create_hu_moments(MHI)
+                    mu_mei, v_mei = util.create_hu_moments(MEI)
+
+                    MHIs.append(MHI)
+                    MEIs.append(MEI)
+
+                    mus_mhi.append(mu_mhi)
+                    vs_mhi.append(v_mhi)
+                    mus_mei.append(mu_mei)
+                    vs_mei.append(v_mei)
+
+                    labels.append(num)
+
+    labels = np.array(labels).astype(np.int)
+    mus_mhi = np.array(mus_mhi).astype(np.float32)
+    vs_mhi = np.array(vs_mhi).astype(np.float32)
+    mus_mei = np.array(mus_mei).astype(np.float32)
+    vs_mei = np.array(vs_mei).astype(np.float32)
+
+    return labels, MHIs, MEIs, mus_mhi, vs_mhi, mus_mei, vs_mei
+
+
+"""
+1. Build Dataset
+2. Save it as numpy 
+3. Load from numpy
+4. create KNN
+5. train model
+    -> Here I am just using scale invariant features - v
+6. create features from test video
+7. test model
+8. create output video
+9. create confusion matrix
+
+"""
+# data_loaded = build_dataset(actions, frames)
+
+# 1. build dataset
+fields = ["labels","MHI","MEI", "mus_mhi","vs_mhi","mus_mei","vs_mei"]
+# data = build_entire_dataset(actions)
+# if debug:
+#     print("1. Build Dataset - Done")
+#
+# # 2. Save it as numpy
+# if not os.path.exists("Data NPY Different T"):
+#     os.makedirs("Data NPY Different T")
+# for i, filename in enumerate(fields):
+#     np.save(os.path.join("./Data NPY Different T/"+filename+".npy"), data[i])
+# if debug:
+#     print("2. Save it as numpy - Done")
+
+#3. Load from numpy
+data_loaded = []
+for field in fields:
+    np_data = np.load(os.path.join("Data NPY Different T",field+".npy"))
+    data_loaded.append(np_data)
+
+if debug:
+    print("3. Load Dataset from Numpy - Done")
+
+# 4. create KNN
+# reference https://towardsdatascience.com/building-a-k-nearest-neighbors-k-nn-model-with-scikit-learn-51209555453a
+knn_cv = KNeighborsClassifier(n_neighbors=25)
+knn_cv.fit(data_loaded[3],data_loaded[0])
+if debug:
+    print("4.  create KNN - Done")
+
+# 5. train model with cv of 5
+print(data_loaded[0].shape)
+cv_scores = cross_val_score(knn_cv, data_loaded[3],data_loaded[0], cv=100)
 #print each cv score (accuracy) and average them
-print(cv_scores)
-print("cv_scores mean:{}".format(np.mean(cv_scores)))
+if debug:
+    print("5. train model with cv of 5 - Done")
+    print(cv_scores)
+    print("cv_scores mean:{}".format(np.mean(cv_scores)))
+
+# 6. create features from test video
+#start_frame = [203, 380, 520, 809, 1028, 1228]
+start_frame = [1]
+end_frame = [108]
+#end_frame = [273, 447, 600, 900, 1062, 1271]
+# # Th needs to be chosen for each action
+# Th = [5, 55, 50, 50, 70, 60]
+# # T needs to be chosen and differs with actions
+# T = [40, 60, 60, 60, 60, 60]
+
+test_video_path = os.path.join(IN_DIR, "person15_boxing_d1_uncomp.avi")
+mu, v = util.get_features(test_video_path, start_frame, end_frame, Th, T)
+if debug:
+    print("6. create features from test video - Done")
+    print(v)
+
+# 7. test model
+predictions = knn_cv.predict(v)
+if debug:
+    print("7. test model - Done")
+    print(predictions)
+
+# 8. create output video
+frame_ids = []
+actions = {1: "boxing",
+           2: "handclapping",
+           3: "handwaving",
+           4: " jogging",
+           5: "running",
+           6: "walking"}
+output_path = util.create_video_output(test_video_path, "boxing", actions, predictions, start_frame, end_frame, frame_ids)
+
+if debug:
+    print("8. create output video - Done")
+    print("Output video is created at {}".format(output_path))
--- MHI Activity Classification/util.py
+++ MHI Activity Classification/util.py
@@ -4,7 +4,10 @@ import numpy as np
 
 
 debug = False
+
+
 #reference: assignment 3
+
 def video_frame_generator(filename):
     video = cv2.VideoCapture(filename)
     while video.isOpened():
@@ -16,10 +19,12 @@ def video_frame_generator(filename):
     video.release()
     yield None
 
+
 def mp4_video_writer(filename, frame_size, fps=20):
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     filename = filename.replace('.mp4', '.avi')
     return cv2.VideoWriter(filename, fourcc, fps, frame_size)
+
 
 def create_binary_images(video_path, k, sigma, start_frame, end_frame, k_size, threshold, folder):
     image_gen = video_frame_generator(video_path)
@@ -73,6 +78,7 @@ def create_binary_images(video_path, k, sigma, start_frame, end_frame, k_size, t
 
     return binaries, reals
 
+
 def create_mhi(binaries, T):
     mhi_t = np.zeros(binaries[0].shape, dtype=np.float)
     for binary in binaries:
@@ -81,6 +87,7 @@ def create_mhi(binaries, T):
         mhi_t = p1 + np.clip(p2, 0, 255)*(binary == 0)
     mhi = mhi_t.astype(np.uint8)
     return mhi
+
 
 def create_hu_moments(img):
     pq = [(2,0), (0,2), (1,1), (1,2),(2,1), (2,2), (3,0), (0,3)]
@@ -104,3 +111,62 @@ def create_hu_moments(img):
         v[i] = mupq/denom
 
     return mu, v
+
+
+def get_features(video_path, start_frame, end_frame, Th, T):
+    mus_mhi = []
+    vs_mhi = []
+
+    for i in range(len(start_frame)):
+        binaries, real = create_binary_images(video_path, k=(5,)*2, sigma=0,
+                                          start_frame=start_frame[i], end_frame=end_frame[i],
+                                          k_size=(3,)*2, threshold=Th[i], folder="test_"+str(i))
+        MHI = create_mhi(binaries, T[i])
+
+        cv2.normalize(MHI, MHI, 0.0, 255.0, cv2.NORM_MINMAX)
+        MEI = (255 * MHI > 0).astype(np.uint8)
+
+        mu_mhi, v_mhi = create_hu_moments(MHI)
+
+        mus_mhi.append(mu_mhi)
+        vs_mhi.append(v_mhi)
+
+    mus_mhi = np.array(mus_mhi).astype(np.float32)
+    vs_mhi = np.array(vs_mhi).astype(np.float32)
+
+    return mus_mhi, vs_mhi
+
+
+def create_video_output(test_video_path, output_name, actions, predictions, start_frame, end_frame, save_frames):
+    output_path = "./output/predicted-{}.avi".format(output_name)
+
+    image_gen = video_frame_generator(test_video_path)
+    img = image_gen.__next__()
+    h, w, d = img.shape
+    point = (int(w/7)-5, int(h/5)+20)
+
+    out = mp4_video_writer(output_path, (w, h), fps=40)
+
+    frame_num = 1
+    i = 0
+    start = start_frame[i]
+    end = end_frame[i]
+
+    while img is not None:
+
+        cv2.putText(img, "(predicted: {})".format(actions[predictions[i]]), point, cv2.FONT_HERSHEY_DUPLEX, 2.5, (255,0,0), 1)
+        out.write(img)
+
+        if frame_num in save_frames:
+            cv2.imwrite("output-{}.png".format(str(frame_num)), img)
+
+        if frame_num == end and len(start_frame)!=i+1:
+            i += 1
+            start = start_frame[i]
+            end = end_frame[i]
+        frame_num += 1
+        img = image_gen.__next__()
+
+    out.release()
+
+    return output_path
